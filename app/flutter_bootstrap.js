9f893a1f42cbae7b3052300697cadb7bb4265a7f
pocha <pocha@kilvish.in>
2026-06-03 22:54:29 +0530

    updating to latest kilvish app

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3516231632" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2383641912" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
